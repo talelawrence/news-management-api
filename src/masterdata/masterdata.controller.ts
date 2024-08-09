eea990ad9aad8e2fb48a39db18c0143ae05daa23
Phubadin Khamngoen <phubadin.k1997@gmail.com>
2024-08-10 01:40:20 +0700

    Refactor newsdata.controller.ts for improved image handling

--- src/masterdata/masterdata.controller.ts
+++ src/masterdata/masterdata.controller.ts
@@ -16,9 +16,10 @@ export class MasterdataController {
   constructor(private readonly masterdataService: MasterdataService) {}
 
   @Get('/main/all')
-  getMainAll() {
+  async getMainAll() {
     try {
-      const result = this.masterdataService.getMainGroupNewsMasterDataAll();
+      const result = await this.masterdataService.getMainGroupNewsMasterDataAll();
+      console.log(result);
       return { success: true, message: "get main group news successful", data: result }
     } catch (err) {
       return { success: false, message: err.message }
@@ -27,9 +28,9 @@ export class MasterdataController {
   }
 
   @Get('/sub/all')
-  getSubAll() {
+  async getSubAll() {
     try {
-      const result = this.masterdataService.getSubGroupNewsMasterDataAll();
+      const result = await this.masterdataService.getSubGroupNewsMasterDataAll();
       return { success: true, message: "get sub group news successful", data: result }
     } catch (err) {
       return { success: false, message: err.message }
@@ -37,9 +38,9 @@ export class MasterdataController {
   }
 
   @Get('/province/all')
-  getProvinceAll() {
+  async getProvinceAll() {
     try {
-      const result = this.masterdataService.getProvinceMasterDataAll();
+      const result = await this.masterdataService.getProvinceMasterDataAll();
       return { success: true, message: "get province successful", data: result }
     } catch (err) {
       return { success: false, message: err.message }
@@ -47,9 +48,9 @@ export class MasterdataController {
   }
 
   @Get('/country/all')
-  getCountryAll() {
+  async getCountryAll() {
     try {
-      const result = this.masterdataService.getCountryMasterDataAll();
+      const result = await this.masterdataService.getCountryMasterDataAll();
       return { success: true, message: "get country successful", data: result }
     } catch (err) {
       return { success: false, message: err.message }
@@ -58,9 +59,9 @@ export class MasterdataController {
   }
 
   @Get('/subdistrict/all')
-  getSubDistrictAll() {
+  async getSubDistrictAll() {
     try {
-      const result = this.masterdataService.getSubDistrictMasterDataAll();
+      const result = await this.masterdataService.getSubDistrictMasterDataAll();
       return { success: true, message: "get sub district successful", data: result }
     } catch (err) {
       return { success: false, message: err.message }
@@ -69,9 +70,9 @@ export class MasterdataController {
   }
 
   @Get('/district/all')
-  getDistrictAll() {
+  async getDistrictAll() {
     try {
-      const result = this.masterdataService.getDistrictMasterDataAll();
+      const result = await this.masterdataService.getDistrictMasterDataAll();
       return { success: true, message: "get district successful", data: result }
     } catch (err) {
       return { success: false, message: err.message } 
@@ -79,9 +80,9 @@ export class MasterdataController {
   }
 
   @Get('/department/all')
-  getDepartementAll() {
+  async getDepartementAll() {
     try {
-      const result = this.masterdataService.getDepartementMasterDataAll();
+      const result = await this.masterdataService.getDepartementMasterDataAll();
       return { success: true, message: "get department successful", data: result }
     } catch (err) {
       return { success: false, message: err.message }
@@ -90,9 +91,9 @@ export class MasterdataController {
   }
 
   @Get('/region/all')
-  getRegionAll() {
+  async getRegionAll() {
     try {
-      const result = this.masterdataService.getRegionMasterDataAll();
+      const result = await this.masterdataService.getRegionMasterDataAll();
       return { success: true, message: "get region successful", data: result }
     } catch (err) {
       return { success: false, message: err.message }
@@ -100,9 +101,9 @@ export class MasterdataController {
   }
 
   @Get('sub/:id')
-  getSubById(id: number) {
+  async getSubById(id: number) {
     try {
-      const result = this.masterdataService.getSubGroupNewsMasterDataById(id);
+      const result = await this.masterdataService.getSubGroupNewsMasterDataById(id);
       return { success: true, message: "get sub group news by id successful", data: result }
     } catch (err) {
       return { success: false, message: err.message }
@@ -110,9 +111,9 @@ export class MasterdataController {
   }
 
   @Post('main/create')
-  createMain(@Body() reqBody: createReq) {
+  async createMain(@Body() reqBody: createReq) {
     try {
-      const result =  this.masterdataService.createMainGroupNews(reqBody);
+      const result = await  this.masterdataService.createMainGroupNews(reqBody);
       return { success: true, message: "create main group news successful", data: result }
     } catch (err) {
         return { success: false, message: err.message }
@@ -120,9 +121,9 @@ export class MasterdataController {
   }
 
   @Post('sub/create')
-  createSub(@Body() reqBody: createReq) {
+  async createSub(@Body() reqBody: createReq) {
     try {
-      const result =  this.masterdataService.createSubGroupNews(reqBody);
+      const result =  await this.masterdataService.createSubGroupNews(reqBody);
       return { success: true, message: "create sub group news successful", data: result }
     } catch (err) {
         return { success: false, message: err.message }
@@ -130,9 +131,9 @@ export class MasterdataController {
   }
 
   @Post('province/create')
-  createProvince(@Body() reqBody: createReq) {
+  async createProvince(@Body() reqBody: createReq) {
     try {
-      const result =  this.masterdataService.createProvince(reqBody);
+      const result =  await this.masterdataService.createProvince(reqBody);
       return { success: true, message: "create province successful", data: result }
     } catch (err) {
         return { success: false, message: err.message }
@@ -141,9 +142,9 @@ export class MasterdataController {
   }
 
   @Post('country/create')
-  createCountry(@Body() reqBody: createReq) {
+  async createCountry(@Body() reqBody: createReq) {
     try {
-      const result =  this.masterdataService.createCountry(reqBody);
+      const result =  await this.masterdataService.createCountry(reqBody);
       return { success: true, message: "create country successful", data: result }
     } catch (err) {
         return { success: false, message: err.message }
@@ -152,9 +153,9 @@ export class MasterdataController {
   }
 
   @Post('subdistrict/create')
-  createSubDistrict(@Body() reqBody: createReq) {
+  async createSubDistrict(@Body() reqBody: createReq) {
     try {
-      const result =  this.masterdataService.createSubDistrict(reqBody);
+      const result =  await this.masterdataService.createSubDistrict(reqBody);
       return { success: true, message: "create sub district successful", data: result }
     } catch (err) {
         return { success: false, message: err.message }
@@ -163,9 +164,9 @@ export class MasterdataController {
   }
 
   @Post('district/create')
-  createDistrict(@Body() reqBody: createReq) {
+  async createDistrict(@Body() reqBody: createReq) {
     try {
-      const result =  this.masterdataService.createDistrict(reqBody);
+      const result =  await this.masterdataService.createDistrict(reqBody);
       return { success: true, message: "create district successful", data: result }
     } catch (err) {
         return { success: false, message: err.message}
@@ -173,9 +174,9 @@ export class MasterdataController {
   }
 
   @Post('department/create')
-  createDepartement(@Body() reqBody: createReq) {
+  async createDepartement(@Body() reqBody: createReq) {
     try {
-      const result =  this.masterdataService.createDepartement(reqBody);
+      const result =  await this.masterdataService.createDepartement(reqBody);
       return { success: true, message: "create department successful", data: result }
     } catch (err) {
         return { success: false, message: err.message }
@@ -183,9 +184,9 @@ export class MasterdataController {
   }
 
   @Post('region/create')
-  createRegion(@Body() reqBody: createReq) {
+  async createRegion(@Body() reqBody: createReq) {
     try {
-      const result =  this.masterdataService.createRegion(reqBody);
+      const result =  await this.masterdataService.createRegion(reqBody);
       return { success: true, message: "create region successful", data: result }
     } catch (err) {
         return { success: false, message: err.message }
@@ -193,9 +194,9 @@ export class MasterdataController {
   }
 
   @Put('main/:id')
-  updateMain(@Body() reqBody: createReq,@Param('id') id: number) {
+  async updateMain(@Body() reqBody: createReq,@Param('id') id: number) {
     try {
-      const result =  this.masterdataService.updateMainGroupNews(reqBody, id);
+      const result =  await this.masterdataService.updateMainGroupNews(reqBody, id);
       return { success: true, message: "update main group news successful" , data: result}
     } catch (err) {
         return { success: false, message: err.message }
@@ -203,9 +204,9 @@ export class MasterdataController {
   }
 
   @Put('sub/:id')
-  updateSub(@Body() reqBody: createReq,@Param('id') id: number) {
+  async updateSub(@Body() reqBody: createReq,@Param('id') id: number) {
     try {
-      const result =  this.masterdataService.updateSubGroupNews(reqBody, id);
+      const result =  await this.masterdataService.updateSubGroupNews(reqBody, id);
       return { success: true, message: "update sub group news successful" , data: result}
     } catch (err) {
         return { success: false, message: err.message }
@@ -213,9 +214,9 @@ export class MasterdataController {
   }
 
   @Put('province/:id')
-  updateProvince(@Body() reqBody: createReq,@Param('id') id: number) {
+  async  updateProvince(@Body() reqBody: createReq,@Param('id') id: number) {
     try {
-      const result =  this.masterdataService.updateProvince(reqBody, id);
+      const result =  await this.masterdataService.updateProvince(reqBody, id);
       return { success: true, message: "update province successful" , data: result}
     } catch (err) {
         return { success: false, message: err.message }
@@ -223,9 +224,9 @@ export class MasterdataController {
   }
 
   @Put('country/:id')
-  updateCountry(@Body() reqBody: createReq,@Param('id') id: number) {
+  async updateCountry(@Body() reqBody: createReq,@Param('id') id: number) {
     try {
-      const result =  this.masterdataService.updateCountry(reqBody, id);
+      const result =  await this.masterdataService.updateCountry(reqBody, id);
       return { success: true, message: "update country successful" , data: result}
     } catch (err) {
         return { success: false, message: err.message }
@@ -233,9 +234,9 @@ export class MasterdataController {
   }
 
   @Put('subdistrict/:id')
-  updateSubDistrict(@Body() reqBody: createReq,@Param('id') id: number) {
+  async updateSubDistrict(@Body() reqBody: createReq,@Param('id') id: number) {
     try {
-      const result =  this.masterdataService.updateSubDistrict(reqBody, id);
+      const result =  await this.masterdataService.updateSubDistrict(reqBody, id);
       return { success: true, message: "update sub district successful" , data: result}
     } catch (err) {
         return { success: false, message: err.message }
@@ -243,9 +244,9 @@ export class MasterdataController {
   }
 
   @Put('district/:id')
-  updateDistrict(@Body() reqBody: createReq,@Param('id') id: number) {
+  async updateDistrict(@Body() reqBody: createReq,@Param('id') id: number) {
     try {
-      const result =  this.masterdataService.updateDistrict(reqBody, id);
+      const result =  await this.masterdataService.updateDistrict(reqBody, id);
       return { success: true, message: "update district successful" , data: result}
     } catch (err) {
         return { success: false, message: err.message }
@@ -253,9 +254,9 @@ export class MasterdataController {
   }
 
   @Put('department/:id')
-  updateDepartement(@Body() reqBody: createReq,@Param('id') id: number) {
+  async updateDepartement(@Body() reqBody: createReq,@Param('id') id: number) {
     try {
-      const result =  this.masterdataService.updateDepartement(reqBody, id);
+      const result =  await this.masterdataService.updateDepartement(reqBody, id);
       return { success: true, message: "update department successful" , data: result}
     } catch (err) {
         return { success: false, message: err.message }
@@ -263,9 +264,9 @@ export class MasterdataController {
   }
 
   @Put('region/:id')
-  updateRegion(@Body() reqBody: createReq,@Param('id') id: number) {
+  async updateRegion(@Body() reqBody: createReq,@Param('id') id: number) {
     try {
-      const result =  this.masterdataService.updateRegion(reqBody, id);
+      const result = await this.masterdataService.updateRegion(reqBody, id);
       return { success: true, message: "update region successful" , data: result}
     } catch (err) {
         return { success: false, message: err.message }
